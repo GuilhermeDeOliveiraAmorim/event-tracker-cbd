78d7d0baf772babf7dc7009758eaa4d2f3a0c2ec
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-05-30 06:07:16 -0300

    feat: remove eventos from app

--- src/App.tsx
+++ src/App.tsx
@@ -14,23 +14,25 @@ function App() {
 
     const [filtro, setFiltro] = useState<Date | null>();
 
-    // const adicionarEvento = (evento: IEvento) => {
-    //     evento.id = Math.round(new Date().getTime() / 1000);
-    //     eventos.push(evento);
-    //     console.log(eventos);
+    const adicionarEvento = (evento: IEvento) => {
+        evento.id = Math.round(new Date().getTime() / 1000);
+        // eventos.push(evento);
+        // console.log(eventos);
 
-    //     setEventos([...eventos]);
-    // };
-    // const alterarStatusEvento = (id: number) => {
-    //     const evento = eventos.find((evento) => evento.id === id);
-    //     if (evento) {
-    //         evento.completo = !evento.completo;
-    //     }
-    //     setEventos([...eventos]);
-    // };
-    // const deletarEvento = (id: number) => {
-    //     setEventos([...eventos.filter((evento) => evento.id !== id)]);
-    // };
+        // setEventos([...eventos]);
+    };
+
+    const alterarStatusEvento = (id: number) => {
+        // const evento = eventos.find((evento) => evento.id === id);
+        // if (evento) {
+        //     evento.completo = !evento.completo;
+        // }
+        // setEventos([...eventos]);
+    };
+
+    const deletarEvento = (id: number) => {
+        // setEventos([...eventos.filter((evento) => evento.id !== id)]);
+    };
 
     const aplicarFiltro = (data: Date | null) => {
         setFiltro(data);
@@ -57,12 +59,11 @@ function App() {
                             aoFiltroAplicado={aplicarFiltro}
                             aoAlterarStatus={alterarStatusEvento}
                             aoDeletarEvento={deletarEvento}
-                            eventos={filtrados}
                         />
                     </Card>
                 </div>
                 <div className={style.Coluna}>
-                    <Calendario eventos={eventos} />
+                    <Calendario />
                 </div>
             </div>
         </RecoilRoot>
